8582668c64d29535067f70a0cbaf5d8dd17af9d9
lyukidon <64675543+lyukidon@users.noreply.github.com>
2022-12-16 20:45:15 +0900

    fix not updating when the position is changed

--- src/App.js
+++ src/App.js
@@ -17,50 +17,43 @@ function App() {
     const drawCharacter = () => {
         const canvas = canvasRef.current;
         const ctx = canvas.getContext('2d');
-        ctx.strokeRect(0, 0, 20, 20);
+        clearCanvas();
+        ctx.strokeRect(pos.x, pos.y, 20, 20);
     };
     const moveCharacter = (e) => {
+        console.log('key')
         const canvas = canvasRef.current;
         const ctx = canvas.getContext('2d');
         switch (e.keyCode) {
+            // right
             case 39:
-                setPos({
-                    ...pos,
+                setPos((prev) => ({
+                    ...prev,
                     x: pos.x + 10,
-                });
-                clearCanvas();
-                ctx.strokeRect(pos.x, pos.y, 20, 20);
+                }));
                 break;
+            // Left
             case 37:
-                setPos({
-                    ...pos,
+                setPos((prev) => ({
+                    ...prev,
                     x: pos.x - 10,
-                });
-                clearCanvas();
-                ctx.strokeRect(pos.x, pos.y, 20, 20);
+                }));
                 break;
+            // Up
             case 38:
-                setPos({
-                    ...pos,
+                setPos((prev) => ({
+                    ...prev,
                     y: pos.y - 10,
-                });
-                clearCanvas();
-                ctx.strokeRect(pos.x, pos.y, 20, 20);
+                }));
                 break;
+            // Down
             case 40:
-                setPos({
-                    ...pos,
+                setPos((prev) => ({
+                    ...prev,
                     y: pos.y + 10,
-                });
-                clearCanvas();
-                ctx.strokeRect(pos.x, pos.y, 20, 20);
+                }));
                 break;
         }
-        // right
-        // Left
-
-        // Up
-        // Down
     };
     useEffect(() => {
         clearCanvas();
@@ -68,9 +61,12 @@ function App() {
         drawCharacter();
     }, []);
     useEffect(() => {
+        console.log('useEffect-pos');
         document.addEventListener('keydown', moveCharacter);
+        drawCharacter();
         return () => {
             document.removeEventListener('keydown', moveCharacter);
+            drawCharacter();
         };
     }, [pos]);
     return (
