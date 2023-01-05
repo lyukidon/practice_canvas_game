3e7b214d6beb73a4cde3241f9b9a05da23edf59d
lyukidon <qgx99098@gmail.com>
2023-01-05 21:52:06 +0900

    add timer

--- src/App.js
+++ src/App.js
@@ -1,13 +1,25 @@
 import { useState, useRef, useEffect } from "react";
 import GameRenderer from "./utils/GameRenderer";
 import { eventCharacter } from "./utils/Event";
-// import right_png from "./assets/image/right.png";
-// import bomb_png from "./assets/image/bomb.png";
 
 function App() {
+    const [timer, setTimer] = useState(3);
+    const timerRef = useRef(timer);
+
     const canvasRef = useRef(null);
     const [pos, setPos] = useState(0);
     const [dropBombData, setBombData] = useState([]);
+
+    useEffect(() => {
+        let reduceTimer = setInterval(() => {
+            timerRef.current--;
+            setTimer((prev) => prev-1);
+            if (timerRef.current === 0) {
+                clearInterval(reduceTimer);
+            }
+        },1000);
+        return ()=>clearInterval(reduceTimer)
+    }, []);
 
     useEffect(() => {
         const canvas = canvasRef.current;
@@ -15,13 +27,7 @@ function App() {
         GameRenderer(canvas, ctx, pos, dropBombData);
 
         const eventChar = (evt) =>
-            eventCharacter(
-                pos,
-                setPos,
-                dropBombData,
-                setBombData,
-                evt
-            );
+            eventCharacter(pos, setPos, dropBombData, setBombData, evt);
         document.addEventListener("keydown", eventChar);
         return () => {
             document.removeEventListener("keydown", eventChar);
@@ -31,6 +37,13 @@ function App() {
     return (
         <div>
             <canvas ref={canvasRef} width={900} height={600} />
+            <div>
+                <div>Timer</div>
+                <div>
+                    {parseInt(timer / 60)} :{" "}
+                    {timer % 60 < 10 ? "0" + (timer % 60) : timer % 60}
+                </div>
+            </div>
         </div>
     );
 }
